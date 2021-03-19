4d0a3630927dcb567e7acf77f471c909b136754f
en2 <ernest.szczepaniak.en2@gmail.com>
2021-03-19 13:52:33 +0100

    dodanie vectora jako entry przy linkowaniu

--- cmake/CMakeConfigCompilation.cmake
+++ cmake/CMakeConfigCompilation.cmake
@@ -18,7 +18,7 @@ set(private_definition_host     )
 # ---------------------------------------------| option |--------------------------------------------- #
 
 set(public_option_common        )
-set(public_option_target        -gstrict-dwarf)
+set(public_option_target        )
 set(public_option_host          )
 
 set(private_option_common       )
@@ -48,7 +48,8 @@ set(public_link_target          --specs=nosys.specs -nostartfiles
                                 -Wl,--defsym=__ocram_start=${ocram_start_cached}
                                 -Wl,--defsym=__ocram_size=${ocram_size_cached}
                                 -Wl,--defsym=__ocram_size_stack=${ocram_size_stack_cached}
-                                -Wl,--defsym=__ocram_size_pool=${ocram_size_pool_cached})
+                                -Wl,--defsym=__ocram_size_pool=${ocram_size_pool_cached}
+                                -Wl,--entry=__vector_start)
 set(public_link_host            )
 
 set(private_link_common         )
